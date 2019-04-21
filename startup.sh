d9f77e17baec52742dd80bba0d0536e993e2b899
Hayden Beadles <hbeadles@instructure.com>
2019-04-20 21:34:34 -0600

    Master - Add comments to Dockerfiles, startup script

--- startup.sh
+++ startup.sh
@@ -5,11 +5,11 @@ IFS=$'\n\t'
 # Run a case command
 case $1 in
     server)
-
+    # Pull in Docker env
     WEBSITE_BASE_NAME="$(printenv WEBSITE_NAME)"
     BASE_PATH=/var/www/${WEBSITE_BASE_NAME}
 
-
+    # Check if requirements.txt exists, and there is no env already built
     if [ -e $BASE_PATH/requirements.txt ] && [ ! -d $BASE_PATH/env ]; then
         sudo pip3 install virtualenv
         cd $BASE_PATH
@@ -19,14 +19,16 @@ case $1 in
         else
             source $BASE_PATH/env/bin/activate
     fi
-
+    # Change to $BASE_PATH
     cd $BASE_PATH
+    # Make django migrations
     python manage.py makemigrations
     python manage.py migrate
     echo "Made Migrations..."
+    # Add collect static
     python manage.py collectstatic --noinput
+    # Startup apache2 server
     sudo apache2ctl -D FOREGROUND
-#    echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell
     ;;
 
 esac
