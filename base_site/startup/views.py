c2db8109e925c6dbb561200ece318594eef2cc8a
Hayden Beadles <hbeadles@instructure.com>
2019-04-21 15:37:11 -0600

    Master - Modify startup module to correctly refer to startup in the view"

--- base_site/startup/views.py
+++ base_site/startup/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the startup index.")
